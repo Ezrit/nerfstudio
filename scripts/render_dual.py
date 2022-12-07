8750353528991e48028addcf375c26e471a35d4e
Moritz Muehlhausen <moritz.muehlhausen@web.de>
2022-12-07 16:03:26 +0900

    can render the same path for 2 nerfs for now

--- scripts/render_dual.py
+++ scripts/render_dual.py
@@ -25,7 +25,10 @@ from typing_extensions import assert_never
 
 from nerfstudio.cameras.camera_paths import get_path_from_json, get_spiral_path
 from nerfstudio.cameras.cameras import Cameras
+from nerfstudio.configs import base_config as cfg
 from nerfstudio.configs.base_config import Config  # pylint: disable=unused-import
+from nerfstudio.data.dataparsers.stellav_dataparser import StellaVSlamDataParserConfig
+from nerfstudio.data.utils.datasets import InputDataset
 from nerfstudio.pipelines.base_pipeline import Pipeline
 from nerfstudio.utils import install_checks
 from nerfstudio.utils.eval_utils import eval_setup
@@ -93,8 +96,10 @@ class DualRenderTrajectory:
     main_load_config: Path
     # Path to second config YAML file.
     sub_load_config: Path
-    # Trajectory to render.
-    traj: Literal["spiral", "interp", "filename"] = "filename"
+    # Name of the renderer output to use. rgb, depth, etc.
+    rendered_output_name: str = "rgb"
+    # Trajectory to render. Currently only filename trajectory allowed!
+    traj: Literal["filename"] = "filename"
     # Scaling factor to apply to the camera image resolution.
     downscale_factor: int = 1
     # Filename of the camera path to render.
@@ -114,9 +119,77 @@ class DualRenderTrajectory:
 
     def main(self) -> None:
         """Main function."""
-        _, main_pipeline, _ = eval_setup(
+        main_config, main_pipeline, _ = eval_setup(
             self.main_load_config,
             eval_num_rays_per_chunk=self.eval_num_rays_per_chunk,
+        )
+
+        main_dataparser = main_config.pipeline.datamanager.dataparser.setup()
+        _ = main_dataparser.get_dataparser_outputs(split="train")
+
+        install_checks.check_ffmpeg_installed()
+        seconds = self.seconds
+
+        # TODO(ethan): use camera information from parsing args
+
+        if self.traj == "filename":
+            with open(self.camera_path_filename, "r", encoding="utf-8") as f:
+                camera_path = json.load(f)
+            seconds = camera_path["seconds"]
+            camera_path = get_path_from_json(camera_path)
+        else:
+            assert_never(self.traj)
+
+        # Render path in main NeRF
+        main_output_path = Path(self.output_path.parent) / 'main_{name}'.format(name=self.output_path.name)
+        _render_trajectory_video(
+            main_pipeline,
+            camera_path,
+            output_filename=main_output_path,
+            rendered_output_name=self.rendered_output_name,
+            rendered_resolution_scaling_factor=1.0 / self.downscale_factor,
+            seconds=seconds,
+        )
+
+        # load the Dataset from main NeRF to get the transformation matrix
+        # TODO
+        main_transform = main_dataparser.transform
+
+        del main_config, main_pipeline
+
+        # load sub config & pipeline
+        sub_config, sub_pipeline, _ = eval_setup(
+            self.main_load_config,
+            eval_num_rays_per_chunk=self.eval_num_rays_per_chunk,
+        )
+        sub_dataparser = sub_config.pipeline.datamanager.dataparser.setup()
+        _ = sub_dataparser.get_dataparser_outputs(split="train")
+
+        # load the dataset from the seub NeRF to get their transformation matrix
+        # TODO
+        sub_transform = sub_dataparser.transform
+
+        # transform the camera_path from the main coordinate system to the sub coordinate system
+        # first multiply the c2w matrizes with the inverse of the main_transform to get them in stellav coordinates
+        # then multiply this with the sub_transform to get them in sub-NeRF coordinates
+
+        # extract the cameras c2w from main expand them to 'cameras'x4x4  
+        main_cameras_c2w = torch.cat((camera_path.camera_to_worlds, torch.tensor([0, 0, 0, 1]).view(1, 1, 4).expand(camera_path._num_cameras, -1, -1)), 1)
+        # multiply transform matrizes accordingly to get cameras c2w for sub
+        sub_cameras_c2w = torch.matmul(sub_transform, torch.matmul(torch.inverse(main_transform), main_cameras_c2w))
+        #
+
+        camera_path.camera_to_worlds = sub_cameras_c2w[:, :3, :]
+
+        # Render path in main NeRF
+        sub_output_path = Path(self.output_path.parent) / 'sub_{name}'.format(name=self.output_path.name)
+        _render_trajectory_video(
+            sub_pipeline,
+            camera_path,
+            output_filename=sub_output_path,
+            rendered_output_name=self.rendered_output_name,
+            rendered_resolution_scaling_factor=1.0 / self.downscale_factor,
+            seconds=seconds,
         )
         pass
 
