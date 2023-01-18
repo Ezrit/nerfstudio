d7d41d18f39292423304a7971c0429003c662064
Moritz Muehlhausen <moritz.muehlhausen@web.de>
2023-01-18 18:07:40 +0900

    reverted standard nerfacto

--- nerfstudio/models/nerfacto.py
+++ nerfstudio/models/nerfacto.py
@@ -41,7 +41,6 @@ from nerfstudio.fields.density_fields import HashMLPDensityField
 from nerfstudio.fields.nerfacto_field import TCNNNerfactoField
 from nerfstudio.model_components.losses import (
     MSELoss,
-    MSEWLoss,
     distortion_loss,
     interlevel_loss,
     orientation_loss,
@@ -190,7 +189,6 @@ class NerfactoModel(Model):
 
         # losses
         self.rgb_loss = MSELoss()
-        self.rgbw_loss = MSEWLoss
 
         # metrics
         self.psnr = PeakSignalNoiseRatio(data_range=1.0)
@@ -287,9 +285,7 @@ class NerfactoModel(Model):
     def get_loss_dict(self, outputs, batch, metrics_dict=None):
         loss_dict = {}
         image = batch["image"].to(self.device)
-        # loss_dict["rgb_loss"] = self.rgb_loss(image, outputs["rgb"])
-        weight = batch["weight"].to(self.device) if "weight" in batch else torch.ones_like(image)
-        loss_dict["rgbw_loss"] = self.rgbw_loss(image, outputs["rgb"], weight)
+        loss_dict["rgb_loss"] = self.rgb_loss(image, outputs["rgb"])
         if self.training:
             loss_dict["interlevel_loss"] = self.config.interlevel_loss_mult * interlevel_loss(
                 outputs["weights_list"], outputs["ray_samples_list"]
